00a9ebd58e1ecf0110130bac9ad32710aea0650e
Emmanuel BAILLARD <embailla@e2r11p22.42.fr>
2016-02-24 20:20:50 +0100

    git

---
+++ Dropbox/git/embailla/ft_strnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: embailla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/13 13:33:14 by embailla          #+#    #+#             */
+/*   Updated: 2016/02/24 19:37:33 by embailla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	char *str;
+
+	str = (char *)malloc(size * sizeof(*str));
+	if (str == NULL)
+		return (NULL);
+	ft_strclr(str);
+	return (str);
+}
