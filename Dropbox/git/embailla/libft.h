cb10804d4ac66f0ebd799f9c5721c7d4d18ad01c
Emmanuel BAILLARD <embailla@e2r11p22.42.fr>
2016-02-25 17:12:11 +0100

    rendu libft

--- Dropbox/git/embailla/libft.h
+++ Dropbox/git/embailla/libft.h
@@ -6,7 +6,7 @@
 /*   By: embailla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/19 16:26:34 by embailla          #+#    #+#             */
-/*   Updated: 2016/02/24 20:16:16 by embailla         ###   ########.fr       */
+/*   Updated: 2016/02/25 16:58:53 by embailla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,16 @@ typedef struct		s_list
 	struct s_list	*next;
 }					t_list;
 
+char				*ft_strlow(char *s);
+char				*ft_strup(char *s);
+int					ft_abs(int var);
+void				ft_lstadd_end(t_list **alst, t_list *new);
+t_list				*ft_lstnew(void const *content, size_t content_size);
+void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 int					ft_isspace(int c);
 void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
